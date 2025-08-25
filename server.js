f4322cd92fa8fdefaef78edc43cb136cd058b9f3
Mo-Ha-0 <hamad.mohamad.2004@gmail.com>
2025-08-25 15:25:18 +0300

    s

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
