ebb4a1fddbecaa66f4ca59a76ca07eb77fba3dc3
Mo-Ha-0 <hamad.mohamad.2004@gmail.com>
2025-08-25 16:31:04 +0300

    mm

--- knexfile.js
+++ knexfile.js
@@ -12,26 +12,8 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: {
-            host:
-                process.env.DB_HOST ||
-                'dpg-d2m5ndf5r7bs73ecdru0-a.oregon-postgres.render.com',
-            port: process.env.DB_PORT || 5432,
-            user: process.env.DB_USER || 'mohammad',
-            password: process.env.DB_PASS,
-            database: process.env.DB_NAME || 'school_n38e',
-            ssl: { rejectUnauthorized: false },
-        },
-        pool: {
-            min: 0,
-            max: 10,
-            acquireTimeoutMillis: 30000,
-            createTimeoutMillis: 30000,
-            destroyTimeoutMillis: 5000,
-            idleTimeoutMillis: 30000,
-            reapIntervalMillis: 1000,
-            createRetryIntervalMillis: 100,
-        },
+        connection:
+            'postgresql://mohammad:MqdoLaFOIDjLJCxwBMJ3D90D4wWGxkLw@dpg-d2m5ndf5r7bs73ecdru0-a/school_n38e?sslmode=require',
     },
 
     migrations: {
