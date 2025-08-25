2cfb48cfc49763db50cb3c877e9c8bb3462f82f6
Mo-Ha-0 <hamad.mohamad.2004@gmail.com>
2025-08-25 16:08:10 +0300

    a

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
 module.exports = {
     development: {
+        client: 'pg',
         connection: {
             host: 'dpg-d2m5ndf5r7bs73ecdru0-a.oregon-postgres.render.com',
             port: 5432,
             user: 'mohammad',
             password: process.env.DB_PASS,
             database: 'school_n38e',
+        },
+    },
+    production: {
+        client: 'pg',
+        connection: {
+            host:
+                process.env.DB_HOST ||
+                'dpg-d2m5ndf5r7bs73ecdru0-a.oregon-postgres.render.com',
+            port: process.env.DB_PORT || 5432,
+            user: process.env.DB_USER || 'mohammad',
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME || 'school_n38e',
+            ssl: { rejectUnauthorized: false },
         },
     },
 
